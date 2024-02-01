00a1f184c0eb5e644a4341721c23b5dca64e2758
Lior <liorbarak99@gmail.com>
2024-02-01 21:46:28 +0200

    Update Client with menu and some more.

--- Client/Client.cpp
+++ Client/Client.cpp
@@ -1,84 +1,123 @@
+#define _CRT_SECURE_NO_WARNINGS
 #define _WINSOCK_DEPRECATED_NO_WARNINGS
 #include <iostream>
 using namespace std;
 #pragma comment(lib, "Ws2_32.lib")
 #include <winsock2.h> 
 #include <string.h>
+#include <string>
 
 #define TIME_PORT	27015
 
 void main()
 {
+	bool run = true;
+	int ClientInput;
 
-	// Initialize Winsock (Windows Sockets).
-
-	WSAData wsaData;
-	if (NO_ERROR != WSAStartup(MAKEWORD(2, 2), &wsaData))
+	while (run)
 	{
-		cout << "Time Client: Error at WSAStartup()\n";
-		return;
-	}
+		cout << "Please type a number for action you want to ask the server:\n" <<
+			"1-GetTime\n" <<
+			"2-GetTimeWithoutDate\n" <<
+			"3-GetTimeSinceEpoch\n" <<
+			"4-GetClientToServerDelayEstimation\n" <<
+			"5-MeasureRTT\n" <<
+			"6-GetTimeWithoutDateOrSeconds\n" <<
+			"7-GetYear\n" <<
+			"8-GetMonthAndDay\n" <<
+			"9-GetSecondsSinceBeginingOfMonth\n" <<
+			"10-GetWeekOfYear\n" <<
+			"11-GetDaylightSavings \n" <<
+			"12-GetTimeWithoutDateInCity \n" <<
+			"13-MeasureTimeLap \n" <<
+			"14-Exit\n" <<
+			"Your choice is: ";
 
-	// Client side:
-	// Create a socket and connect to an internet address.
+		cin >> ClientInput;
 
-	SOCKET connSocket = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
-	if (INVALID_SOCKET == connSocket)
-	{
-		cout << "Time Client: Error at socket(): " << WSAGetLastError() << endl;
-		WSACleanup();
-		return;
-	}
+		if (ClientInput == 14)
+			exit(0);
 
-	// For a client to communicate on a network, it must connect to a server.
+		cout << "\n";
 
-	// Need to assemble the required data for connection in sockaddr structure.
+		// Initialize Winsock (Windows Sockets).
 
-	// Create a sockaddr_in object called server. 
-	sockaddr_in server;
-	server.sin_family = AF_INET;
-	server.sin_addr.s_addr = inet_addr("127.0.0.1");
-	server.sin_port = htons(TIME_PORT);
+		WSAData wsaData;
+		if (NO_ERROR != WSAStartup(MAKEWORD(2, 2), &wsaData))
+		{
+			cout << "Time Client: Error at WSAStartup()\n";
+			return;
+		}
 
-	// Send and receive data.
+		// Client side:
+		// Create a socket and connect to an internet address.
 
-	int bytesSent = 0;
-	int bytesRecv = 0;
-	char sendBuff[255] = "What's the time?";
-	char recvBuff[255];
+		SOCKET connSocket = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
+		if (INVALID_SOCKET == connSocket)
+		{
+			cout << "Time Client: Error at socket(): " << WSAGetLastError() << endl;
+			WSACleanup();
+			return;
+		}
 
-	// Asks the server what's the currnet time.
-	// The send function sends data on a connected socket.
-	// The buffer to be sent and its size are needed.
-	// The fourth argument is an idicator specifying the way in which the call is made (0 for default).
-	// The two last arguments hold the details of the server to communicate with. 
-	// NOTE: the last argument should always be the actual size of the client's data-structure (i.e. sizeof(sockaddr)).
-	bytesSent = sendto(connSocket, sendBuff, (int)strlen(sendBuff), 0, (const sockaddr*)&server, sizeof(server));
-	if (SOCKET_ERROR == bytesSent)
-	{
-		cout << "Time Client: Error at sendto(): " << WSAGetLastError() << endl;
+		// For a client to communicate on a network, it must connect to a server.
+
+		// Need to assemble the required data for connection in sockaddr structure.
+
+		// Create a sockaddr_in object called server. 
+		sockaddr_in server;
+		server.sin_family = AF_INET;
+		server.sin_addr.s_addr = inet_addr("127.0.0.1");
+		server.sin_port = htons(TIME_PORT);
+
+		// Send and receive data.
+
+		int bytesSent = 0;
+		int bytesRecv = 0;
+		char sendBuff[255];
+		char recvBuff[255];
+
+
+		string input = to_string(ClientInput);
+		strcpy(sendBuff, input.c_str());
+
+		// Asks the server for data.
+		// The send function sends data on a connected socket.
+		// The buffer to be sent and its size are needed.
+		// The fourth argument is an idicator specifying the way in which the call is made (0 for default).
+		// The two last arguments hold the details of the server to communicate with. 
+		// NOTE: the last argument should always be the actual size of the client's data-structure (i.e. sizeof(sockaddr)).
+		bytesSent = sendto(connSocket, sendBuff, (int)strlen(sendBuff), 0, (const sockaddr*)&server, sizeof(server));
+		if (SOCKET_ERROR == bytesSent)
+		{
+			cout << "Time Client: Error at sendto(): " << WSAGetLastError() << endl;
+			closesocket(connSocket);
+			WSACleanup();
+			return;
+		}
+
+
+		cout << "Time Client: Sent: " << bytesSent << "/" << strlen(sendBuff) << " bytes of \"" << sendBuff << "\" message.\n";
+
+
+		// Gets the server's answer using simple recieve (no need to hold the server's address).
+		bytesRecv = recv(connSocket, recvBuff, 255, 0);
+		if (SOCKET_ERROR == bytesRecv)
+		{
+			cout << "Time Client: Error at recv(): " << WSAGetLastError() << endl;
+			closesocket(connSocket);
+			WSACleanup();
+			return;
+		}
+
+		recvBuff[bytesRecv] = '\0'; // Add the null-terminating to make it a string
+		cout << "Time Client: Recieved: " << bytesRecv << " bytes of \"" << recvBuff << "\" message.\n";
+
+
+		// Closing connections and Winsock.
+		cout << "Time Client: Closing Connection.\n";
 		closesocket(connSocket);
-		WSACleanup();
-		return;
+
+		cout << "\n";
 	}
-	cout << "Time Client: Sent: " << bytesSent << "/" << strlen(sendBuff) << " bytes of \"" << sendBuff << "\" message.\n";
-
-	// Gets the server's answer using simple recieve (no need to hold the server's address).
-	bytesRecv = recv(connSocket, recvBuff, 255, 0);
-	if (SOCKET_ERROR == bytesRecv)
-	{
-		cout << "Time Client: Error at recv(): " << WSAGetLastError() << endl;
-		closesocket(connSocket);
-		WSACleanup();
-		return;
-	}
-
-	recvBuff[bytesRecv] = '\0'; //add the null-terminating to make it a string
-	cout << "Time Client: Recieved: " << bytesRecv << " bytes of \"" << recvBuff << "\" message.\n";
-
-	// Closing connections and Winsock.
-	cout << "Time Client: Closing Connection.\n";
-	closesocket(connSocket);
-
-	system("pause");
 }
