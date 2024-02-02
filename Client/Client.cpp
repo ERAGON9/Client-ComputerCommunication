343ae5e3391c12caa2d4841e65f1742206fdaaf5
Lior <liorbarak99@gmail.com>
2024-02-02 22:43:30 +0200

    Update client.

--- Client/Client.cpp
+++ Client/Client.cpp
@@ -9,69 +9,91 @@ using namespace std;
 
 #define TIME_PORT	27015
 
+void PrintMsg()
+{
+	cout << "Please type a number for action you want to ask the server:\n" <<
+		"1-GetTime\n" <<
+		"2-GetTimeWithoutDate\n" <<
+		"3-GetTimeSinceEpoch\n" <<
+		"4-GetClientToServerDelayEstimation\n" <<
+		"5-MeasureRTT\n" <<
+		"6-GetTimeWithoutDateOrSeconds\n" <<
+		"7-GetYear\n" <<
+		"8-GetMonthAndDay\n" <<
+		"9-GetSecondsSinceBeginingOfMonth\n" <<
+		"10-GetWeekOfYear\n" <<
+		"11-GetDaylightSavings \n" <<
+		"12-GetTimeWithoutDateInCity \n" <<
+		"13-MeasureTimeLap \n" <<
+		"14-Exit\n" <<
+		"Your choice is: ";
+}
+
+void PrintCities()
+{
+	cout << "The available cities are:\n" <<
+		"Doha\n" <<
+		"Prague\n" <<
+		"New-York\n" <<
+		"Berlin\n" <<
+		"Choose one of the cities above, by writing the name of the city.\n" <<
+		"Your choice is: ";
+}
+
 void main()
 {
+	// Initialize Winsock (Windows Sockets).
+	WSAData wsaData;
+	if (NO_ERROR != WSAStartup(MAKEWORD(2, 2), &wsaData))
+	{
+		cout << "Time Client: Error at WSAStartup()\n";
+		return;
+	}
+
+	// Client side:
+	// Create a socket and connect to an internet address.
+	SOCKET connSocket = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
+	if (INVALID_SOCKET == connSocket)
+	{
+		cout << "Time Client: Error at socket(): " << WSAGetLastError() << endl;
+		WSACleanup();
+		return;
+	}
+
+	// For a client to communicate on a network, it must connect to a server.
+	// Need to assemble the required data for connection in sockaddr structure.
+	// Create a sockaddr_in object called server. 
+	sockaddr_in server;
+	server.sin_family = AF_INET;
+	server.sin_addr.s_addr = inet_addr("127.0.0.1");
+	server.sin_port = htons(TIME_PORT);
+
+	// My variables.
 	bool run = true;
 	int ClientInput;
+	string city;
 
 	while (run)
 	{
-		cout << "Please type a number for action you want to ask the server:\n" <<
-			"1-GetTime\n" <<
-			"2-GetTimeWithoutDate\n" <<
-			"3-GetTimeSinceEpoch\n" <<
-			"4-GetClientToServerDelayEstimation\n" <<
-			"5-MeasureRTT\n" <<
-			"6-GetTimeWithoutDateOrSeconds\n" <<
-			"7-GetYear\n" <<
-			"8-GetMonthAndDay\n" <<
-			"9-GetSecondsSinceBeginingOfMonth\n" <<
-			"10-GetWeekOfYear\n" <<
-			"11-GetDaylightSavings \n" <<
-			"12-GetTimeWithoutDateInCity \n" <<
-			"13-MeasureTimeLap \n" <<
-			"14-Exit\n" <<
-			"Your choice is: ";
-
-		cin >> ClientInput;
-
-		if (ClientInput == 14)
-			exit(0);
-
-		cout << "\n";
-
-		// Initialize Winsock (Windows Sockets).
-
-		WSAData wsaData;
-		if (NO_ERROR != WSAStartup(MAKEWORD(2, 2), &wsaData))
+		ClientInput = -1;
+		while (ClientInput < 1 || ClientInput > 14) // Check for correct input. (between 1-14)
 		{
-			cout << "Time Client: Error at WSAStartup()\n";
-			return;
+			PrintMsg();
+			cin >> ClientInput;
+
+			if (ClientInput == 12)
+			{
+				cout << "\n";
+				PrintCities();
+				cin >> city;
+			}
+
+			if (ClientInput == 14)
+				exit(0);
+			cout << "\n";
 		}
-
-		// Client side:
-		// Create a socket and connect to an internet address.
-
-		SOCKET connSocket = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
-		if (INVALID_SOCKET == connSocket)
-		{
-			cout << "Time Client: Error at socket(): " << WSAGetLastError() << endl;
-			WSACleanup();
-			return;
-		}
-
-		// For a client to communicate on a network, it must connect to a server.
-
-		// Need to assemble the required data for connection in sockaddr structure.
-
-		// Create a sockaddr_in object called server. 
-		sockaddr_in server;
-		server.sin_family = AF_INET;
-		server.sin_addr.s_addr = inet_addr("127.0.0.1");
-		server.sin_port = htons(TIME_PORT);
 
 		// Send and receive data.
-
 		int bytesSent = 0;
 		int bytesRecv = 0;
 		char sendBuff[255];
@@ -79,6 +101,9 @@ void main()
 
 
 		string input = to_string(ClientInput);
+		if (ClientInput == 12)
+			input += "-" + city;
+
 		strcpy(sendBuff, input.c_str());
 
 		// Asks the server for data.
@@ -96,9 +121,7 @@ void main()
 			return;
 		}
 
-
 		cout << "Time Client: Sent: " << bytesSent << "/" << strlen(sendBuff) << " bytes of \"" << sendBuff << "\" message.\n";
-
 
 		// Gets the server's answer using simple recieve (no need to hold the server's address).
 		bytesRecv = recv(connSocket, recvBuff, 255, 0);
@@ -112,12 +135,12 @@ void main()
 
 		recvBuff[bytesRecv] = '\0'; // Add the null-terminating to make it a string
 		cout << "Time Client: Recieved: " << bytesRecv << " bytes of \"" << recvBuff << "\" message.\n";
-
-
-		// Closing connections and Winsock.
-		cout << "Time Client: Closing Connection.\n";
-		closesocket(connSocket);
-
-		cout << "\n";
 	}
+
+	// Closing connections and Winsock.
+	cout << "Time Client: Closing Connection.\n";
+	closesocket(connSocket);
+
+	cout << "\n";
 }
+
